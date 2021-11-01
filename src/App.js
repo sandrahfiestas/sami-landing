b55d488ad24d81786b411872aad531e0cc465580
sandrahfiestas <shfiestas@gmail.com>
2021-10-31 20:57:55 -0500

    configure routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from 'react-router-dom';
+import Home from './pages/Home';
 
 function App() {
   return (
-    <>
-      <h1>SAMI</h1>
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route path="/404">
+          <h1>404 Not found</h1>
+        </Route>
+        <Route path="*">
+          <Redirect to="/404" />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
