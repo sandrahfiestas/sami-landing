02d498875fdb32286717ece80ab98642b8ec4c20
sandrahfiestas <shfiestas@gmail.com>
2021-11-02 18:01:25 -0500

    Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,11 +7,12 @@ import Container from 'react-bootstrap/Container';
 import logoTipoIcon from '../images/logo-tipo.svg'
 
 function NavBar() {
+  // cambio de color del navbar con Jquery
   $(function(){
     $(window).scroll(function(){
       var scroll = $(window).scrollTop();
-      if (scroll > 200) {
-        $(".navbar").css("background" , "radial-gradient(173.26% 94.27% at 50% 100%, #F68F44 0%, #EC0F42 37.5%, #E22033 46.35%, #8B112D 100%)");
+      if (scroll > 10) {
+        $(".navbar").css("background" , "#8E122E");
       }
       else{
         $(".navbar").css("background" , "transparent");  	
