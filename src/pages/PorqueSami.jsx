3ee737c1b8700ec052b6b7181c0ba812fd61cdd4
sandrahfiestas <shfiestas@gmail.com>
2021-11-09 12:46:09 -0500

    estilos article - CoreSami

--- src/pages/PorqueSami.jsx
+++ src/pages/PorqueSami.jsx
@@ -6,6 +6,7 @@ import circleIcon from '../images/back-circle.png';
 import puntosIcon from '../images/back-puntos.png';
 import equisIcon from '../images/back-equis.png';
 import logoMiranda from '../images/logo-miranda.png';
+import capacidadesImage from '../images/capacidades.png';
 
 function PorqueSami() {
   return(
@@ -41,15 +42,16 @@ function PorqueSami() {
               </div>
               <button>Solicita una demo</button>
             </section>
-            {/* <section className="content-coreSami">
-              <p className="itemSubtitulo">CORE SAMI</p>
-              <h2 className="subtitulo">Cuenta con el respaldo del Estudio Miranda&Amado</h2>
-              <p className="content-descubreComo-description">Miranda&Amado brinda el soporte legal con un alto nivel de especialización en diversas áreas del derecho y con un conocimiento profundo de las problemáticas del mundo legal laboral, lo cual permite brindar un servicio legal que se integra perfectamente con las estrategias empresariales de nuestros usuarios.</p>
-              <div className="d-flex justify-content-center">
-                <img src={mirandaAmadoImage} alt="imagen Miranda" className="content-descubreComo-imagenMiranda" />
+            <section className="content-coreSami">
+              <div>
+                <p className="itemSubtitulo">CORE SAMI</p>
+                <h2 className="subtitulo">Mejores servicios legales centrados en las personas</h2>
+                <p className="content-coreSami-description">El diseño centrado en el ser humano ha sido una metodología de innovación dominante en las industrias de servicios, desde la medicina hasta los seguros y las finanzas y que hoy sirven también al sistema legal, mejorarando el acceso de las personas a la justicia,  promoviendo la innovación y creando servicios legales más humanos.</p>
               </div>
-              <button>Solicita una demo</button>
-            </section> */}
+              <div className="d-flex justify-content-center">
+                <img src={capacidadesImage} alt="imagen diseño legal" className="content-coreSami-imagenCapacidades" />
+              </div>
+            </section>
           </div>
         </div>
       </main>
